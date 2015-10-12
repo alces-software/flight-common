748230897bd1e315561ce8902a1e76988f11f65c
Ben Armston <ben.armston@alces-software.com>
2015-10-12 11:07:26 +0100

    Header collapses for mobile devices

--- Header.js
+++ Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  ButtonToolbar, Nav, Navbar, NavDropdown, MenuItem
+  ButtonToolbar, CollapsibleNav, Nav, Navbar, NavDropdown, MenuItem
 } from 'react-bootstrap';
 import { Link } from 'react-router';
 import {default as Icon} from 'react-fontawesome';
@@ -10,15 +10,17 @@ import { ButtonLink, NavItemLink } from './Links';
 class Header extends React.Component {
   render() {
     return (
-      <Navbar brand={<Link to="/">Alces Flight</Link>} className="flightNav" fixedTop>
-        <Nav>
-          <NavItemLink to="/start">
-            <Icon name="home"/> Start
-          </NavItemLink>
-        </Nav>
-        <Nav right>
-          { this.props.account ? this.accountNav() : this.annoymousNav() }
-        </Nav>
+      <Navbar brand={<Link to="/">Alces Flight</Link>} className="flightNav" fixedTop toggleNavKey={0}>
+        <CollapsibleNav eventKey={0}>
+          <Nav navbar>
+            <NavItemLink to="/start">
+              <Icon name="home"/> Start
+            </NavItemLink>
+          </Nav>
+          <Nav right navbar>
+            { this.props.account ? this.accountNav() : this.annoymousNav() }
+          </Nav>
+        </CollapsibleNav>
       </Navbar> 
     )
   }
