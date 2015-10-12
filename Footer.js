158573eb29d6d6d5307988b959afe0685cf8841f
Bob Whitelock <bob.whitelock@alces-software.com>
2015-10-12 15:00:25 +0100

    Improve footer styling
    Have copyright and terms of service link appear in one line with some
    padding between; waiting until know more about how Flight should look
    and feel before adding more styling.

--- Footer.js
+++ Footer.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router';
 
-require('styles/footer.css');
+require('styles/footer.scss');
 
 class Footer extends React.Component {
   render() {
     return (
         <footer>
-          <div className="container">
-            <p className="text-muted">Alces Flight &copy; 2015 Alces Software Ltd</p>
-            <Link to="/terms">Terms of Service</Link>
+          <div className="container footer-text">
+            <span className="text-muted">Alces Flight &copy; 2015 Alces Software Ltd</span>
+            <span>
+              <Link to="/terms">Terms of Service</Link>
+            </span>
           </div>
         </footer>
     )
