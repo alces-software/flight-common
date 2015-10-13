f6defdf66debf2c87fe61f90680c77515fee998f
Ben Armston <ben.armston@alces-software.com>
2015-10-13 13:31:33 +0100

    Extract component to display missing notice

---
+++ src/components/MissingNotice.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Jumbotron } from 'react-bootstrap';
+
+require("styles/missingNotice.scss");
+
+export default class MissingNotice extends React.Component {
+  render() {
+    const { title } = this.props;
+
+    return (
+      <Jumbotron className="missingNotice">
+        <h2 className="missingNotice-title">{title}</h2>
+        <div className="missingNotice-body">
+          {this.wrappedChildren()}
+        </div>
+      </Jumbotron>
+    );
+  }
+
+  wrappedChildren() {
+    const { children } = this.props;
+
+    if (React.isValidElement(children)) {
+      return children;
+    } else {
+      return <p>{children}</p>;
+    }
+  }
+}
+
+MissingNotice.propTypes = {
+  title: React.PropTypes.node.isRequired,
+  children: React.PropTypes.node.isRequired,
+};
