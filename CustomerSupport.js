7847a2b541842abb21891e19035dafe27cb99a08
Bob Whitelock <bob.whitelock@alces-software.com>
2015-10-23 11:56:37 +0100

    Add error generating functions
    Currently error messages shown when communication with server not
    possible and server errors; React itself errors when other errors
    received however (will add this next).

---
+++ CustomerSupport.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class CustomerSupport extends React.Component {
+  render() {
+    return (
+      <a href='mailto: support@alces-software.com'>Alces Customer Support</a>
+    );
+  }
+}
