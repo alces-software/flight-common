727037568d9c89c176ac48ca2c1a608370046433
Ben Armston <ben.armston@alces-software.com>
2015-11-11 12:26:16 +0000

    Add nav overlay actions
    Accepting an invitation is now done via a nav overlay action.

--- src/components/nav/NavOverlayEntry.js
+++ src/components/nav/NavOverlayEntry.js
@@ -3,13 +3,20 @@ import {default as Icon} from 'react-fontawesome';
 import moment from 'moment';
 import TimeAgo from 'react-timeago';
 import { OverlayTrigger, Tooltip } from 'react-bootstrap';
+import classNames from 'classnames';
 
 export default class NavOverlayEntry extends React.Component {
   render() {
-    const { children, iconClasses, iconName, timestamp, title } = this.props;
+    const {
+      actions, children, className, iconClasses, iconName, timestamp, title 
+    } = this.props;
+    const classes = classNames(
+      "flightOverlay-entry",
+      {[`flightOverlay-entry-${className}`]: className}
+    );
 
     return (
-      <div className="flightOverlay-entry">
+      <div className={classes}>
         <div className="flightOverlay-entry-icon">
           <Icon name={iconName} className={iconClasses}/>
         </div>
@@ -28,7 +35,26 @@ export default class NavOverlayEntry extends React.Component {
               <TimeAgo date={timestamp} formatter={this.formatTimestamp}/>
             </OverlayTrigger>
           </div>
+          {actions ? actions.map((action, idx) => this.renderControl(action, idx)) : null}
         </div>
+      </div>
+    );
+  }
+
+  renderControl(action, key) {
+    const classes = classNames(
+      "flightOverlay-entry-control",
+      {[`flightOverlay-entry-control-${action.className}`]: action.className}
+    );
+    return (
+      <div className={classes} key={key}>
+        <OverlayTrigger
+          overlay={action.tooltip}
+          placement="bottom"
+          container={this.props.overlayContainer}
+          >
+          <Icon name={action.iconName} onClick={action.onClick}/>
+        </OverlayTrigger>
       </div>
     );
   }
@@ -38,7 +64,15 @@ export default class NavOverlayEntry extends React.Component {
   }
 }
 
+const actionShape = {
+  className: PropTypes.string,
+  iconName: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+  tooltip: PropTypes.element.isRequired
+};
+
 NavOverlayEntry.propTypes = {
+  actions: PropTypes.arrayOf(PropTypes.shape(actionShape).isRequired),
   iconClasses: PropTypes.string,
   iconName: PropTypes.string.isRequired,
   timestamp: PropTypes.string.isRequired,
