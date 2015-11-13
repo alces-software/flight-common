10e965e767a809b35f8504dee22cf5ade4ad1962
Ben Armston <ben.armston@alces-software.com>
2015-11-13 11:54:03 +0000

    Improve header for launch page
    Includes styling for a portal-style small page header. This is not be used at
    the moment as the body of the page doesn't quite fit right with the smaller
    header.

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Jumbotron } from 'react-bootstrap';
+import classNames from 'classnames';
 
 import WrappedWithPara from 'components/WrappedWithPara';
 
 class PageHeader extends React.Component {
   render() {
-    const { header, subheader } = this.props;
+    const { header, size, subheader } = this.props;
+    const classes = classNames("pageHeader", {
+      [`pageHeader-${size}`]: size
+    });
 
     return (
-        <Jumbotron className="pageHeader">
+        <Jumbotron className={classes}>
           <div className="container">
             <h1>{header} <small>{subheader}</small></h1>
             {this.props.children && <WrappedWithPara>{this.props.children}</WrappedWithPara>}
