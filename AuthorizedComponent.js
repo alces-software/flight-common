99626c490d62354db68852c3d343b82951ce15be
Bob Whitelock <bob.whitelock@alces-software.com>
2015-11-19 19:07:02 +0000

    Fix AuthorizedComponent indentation
    To match rest of Aviator codebase.

--- AuthorizedComponent.js
+++ AuthorizedComponent.js
@@ -5,41 +5,41 @@ import {connect} from 'react-redux';
 // TODO comment how this works.
 export function authorize(authorizationFunction, authorizationFailedHandler) {
 
-    class AuthorizedComponent extends React.Component {
-        authorized() {
-            return authorizationFunction(this.props);
-        }
-
-        componentWillMount() {
-            this.handleIfUnauthorized();
-        }
-
-        componentWillReceiveProps() {
-            this.handleIfUnauthorized();
-        }
-
-        handleIfUnauthorized() {
-            if (!this.authorized()) {
-                authorizationFailedHandler.bind(this)();
-
-                // TODO: below from above example, adapt for our use.
-                // let redirectAfterLogin = this.props.location.pathname;
-                // this.props.dispatch(pushState(null, `/login?next=${redirectAfterLogin}`));
-            }
-        }
-
-        render() {
-            return (
-                <div>
-                    { this.authorized() ? this.props.children : null }
-                </div>
-            )
-        }
+  class AuthorizedComponent extends React.Component {
+    authorized() {
+      return authorizationFunction(this.props);
     }
 
-    const mapStateToProps = (state) => ({
-      auth: state.auth
-    });
+    componentWillMount() {
+      this.handleIfUnauthorized();
+    }
 
-    return connect(mapStateToProps)(AuthorizedComponent);
+    componentWillReceiveProps() {
+      this.handleIfUnauthorized();
+    }
+
+    handleIfUnauthorized() {
+      if (!this.authorized()) {
+        authorizationFailedHandler.bind(this)();
+
+        // TODO: below from above example, adapt for our use.
+        // let redirectAfterLogin = this.props.location.pathname;
+        // this.props.dispatch(pushState(null, `/login?next=${redirectAfterLogin}`));
+      }
+    }
+
+    render() {
+      return (
+        <div>
+          { this.authorized() ? this.props.children : null }
+        </div>
+      )
+    }
+  }
+
+  const mapStateToProps = (state) => ({
+    auth: state.auth
+  });
+
+  return connect(mapStateToProps)(AuthorizedComponent);
 }
