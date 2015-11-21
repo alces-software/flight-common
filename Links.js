17486dddab65d847f39d46655e312327aa6beec3
Ben Armston <ben.armston@alces-software.com>
2015-11-21 15:36:11 +0000

    Work around bug with eslint...
    and object rest desctructuring.

--- Links.js
+++ Links.js
@@ -4,24 +4,28 @@ import { LinkContainer } from 'react-router-bootstrap';
 
 export class ButtonLink extends React.Component {
 
-    render() {
-         const { to, ...rest } = this.props;
-         return (
-           <LinkContainer to={to} {...rest}>
-             <Button {...rest}>{this.props.children}</Button>
-           </LinkContainer>
-         );
-    }
+  render() {
+    /* eslint-disable no-redeclare */
+    const { to, ...rest } = this.props;
+    /* eslint-enable no-redeclare */
+    return (
+      <LinkContainer to={to} {...rest}>
+        <Button {...rest}>{this.props.children}</Button>
+      </LinkContainer>
+    );
+  }
 }
 
 
 export class NavItemLink extends React.Component {
-    render() {
-         const { to, ...rest } = this.props;
-         return (
-           <LinkContainer to={to} {...rest}>
-             <NavItem {...rest}>{this.props.children}</NavItem>
-           </LinkContainer>
-         );
-    }
+  render() {
+    /* eslint-disable no-redeclare */
+    const { to, ...rest } = this.props;
+    /* eslint-enable no-redeclare */
+    return (
+      <LinkContainer to={to} {...rest}>
+        <NavItem {...rest}>{this.props.children}</NavItem>
+      </LinkContainer>
+    );
+  }
 }
