02360a44ebc75bda94c8411eac89d22e55153627
Ben Armston <ben.armston@alces-software.com>
2015-12-20 22:14:35 +0000

    Copy to clipboard works

---
+++ Clipboard.js
@@ -0,0 +1,106 @@
+import React from 'react';
+import { Button, OverlayTrigger, Tooltip } from 'react-bootstrap';
+import ClipboardAction from 'clipboard/lib/clipboard-action';
+
+import Icon from 'components/Icon';
+
+export class CopyToClipboardButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {copyState: "notCopied"};
+  }
+
+  handleCopy(payload) {
+    this.setState({copyState: "copied"});
+    if (this.props.onCopy) {
+      this.props.onCopy(payload);
+    }
+  }
+
+  handleCopyFailure(payload) {
+    this.setState({copyState: "failed"});
+    if (this.props.onCopyFailure) {
+      this.props.onCopyFailure(payload);
+    }
+  }
+
+  tooltipText() {
+    switch (this.state.copyState) {
+      case "notCopied":
+        return "Copy to clipboard";
+      case "copied":
+        return "Copied!";
+      case "failed":
+        return "Press Ctrl-C to copy";
+    }
+  }
+
+  render() {
+    return (
+      <OverlayTrigger
+        onExited={() => this.setState({copyState: "notCopied"})}
+        overlay={<Tooltip>{this.tooltipText()}</Tooltip>}
+        placement="bottom"
+        shouldUpdatePosition
+        trigger="hover"
+        >
+        <CopyToClipboard {...this.props} onCopy={this.handleCopy.bind(this)}>
+          <Button><Icon name="clipboard"/></Button>
+        </CopyToClipboard>
+      </OverlayTrigger>
+    )
+  }
+}
+
+
+
+export class CopyToClipboard extends React.Component {
+  propTypes: {
+    text: React.PropTypes.string.isRequired,
+    children: React.PropTypes.element.isRequired,
+    onCopy: React.PropTypes.func
+  }
+
+  constructor(props) {
+    super(props);
+    this.emitter = { emit: this.runCallbacks.bind(this) };
+  }
+
+  componentWillUnmount() {
+    if (this.clipboard) {
+      this.clipboard.destroy();
+    }
+  }
+
+  runCallbacks(event, payload) {
+    if (event === "success") {
+      this.props.onCopy && this.props.onCopy(payload);
+    } else if (event === "error") {
+      this.props.onCopyFailed && this.props.onCopyFailed(payload);
+    }
+  }
+
+  handleCopy() {
+    const {text} = this.props;
+    // Can we create one and keep reusing it?
+    if (this.clipboard) {
+      this.clipboard = null;
+    }
+    this.clipboard = new ClipboardAction({
+      action: "copy",
+      emitter: this.emitter,
+      text: text
+    });
+  }
+
+  render() {
+    /* eslint-disable no-redeclare */
+    const {text, onCopy, children, ...rest} = this.props;
+    /* eslint-enable no-redeclare */
+    const elem = React.Children.only(children)
+    return React.cloneElement(elem, {
+      ...rest,
+      onClick: this.handleCopy.bind(this)
+    });
+  }
+}
