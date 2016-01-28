a84c15ad152ba017a8907e93d03849413cae7521
Ben Armston <ben.armston@alces-software.com>
2016-01-28 21:19:21 +0000

    Add missing copyright notices

--- errorMessageCustomization.js
+++ errorMessageCustomization.js
@@ -1,3 +1,10 @@
+/*=============================================================================
+ * Copyright (C) 2015 Stephen F. Norledge and Alces Software Ltd.
+ *
+ * This file is part of Alces Flight.
+ *
+ * All rights reserved, see LICENSE.txt.
+ *===========================================================================*/
 import React from 'react';
 
 import * as authActionTypes from 'auth/actionTypes';
