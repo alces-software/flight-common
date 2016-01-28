614307c19617234fc368095c5c7669c365763228
Ben Armston <ben.armston@alces-software.com>
2016-01-28 21:19:21 +0000

    Add missing copyright notices

--- console.js
+++ console.js
@@ -1,3 +1,10 @@
+/*=============================================================================
+ * Copyright (C) 2015 Stephen F. Norledge and Alces Software Ltd.
+ *
+ * This file is part of Alces Flight.
+ *
+ * All rights reserved, see LICENSE.txt.
+ *===========================================================================*/
 //
 // Defines a wrapper around `console` to make eslint happy and to disable
 // logging in production.
