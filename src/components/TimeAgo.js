37724466d0f81a42fecbbdbcded40664debe1317
Ben Armston <ben.armston@alces-software.com>
2016-01-28 21:19:21 +0000

    Add missing copyright notices

--- src/components/TimeAgo.js
+++ src/components/TimeAgo.js
@@ -1,3 +1,10 @@
+/*=============================================================================
+ * Copyright (C) 2015 Stephen F. Norledge and Alces Software Ltd.
+ *
+ * This file is part of Alces Flight.
+ *
+ * All rights reserved, see LICENSE.txt.
+ *===========================================================================*/
 import React from 'react';
 import {default as ReactTimeAgo} from 'react-timeago';
 import { OverlayTrigger, Tooltip } from 'react-bootstrap';
