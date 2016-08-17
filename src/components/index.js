91bf9e69453d128179874a07fbfc6b49b71de950
Bob Whitelock <bob.whitelock1@gmail.com>
2016-08-17 16:18:40 +0100

    Export the newly added components
    Using the `transform-export-extensions` Babel plugin as well for nicer
    export syntax.

---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+
+export AuthorizedComponent from './AuthorizedComponent';
+export Clipboard from './Clipboard';
+export CustomerSupport from './CustomerSupport';
+export Footer from './Footer';
+// export Header from './Header'; // TODO: doesn't compile yet.
+export Icon from './Icon'
+export Links from './Links';
+export LoadingPage from './LoadingPage';
+export StandardModal from './StandardModal';
+
--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
 
-export const foo = 'hello world'
+export * from 'components';
