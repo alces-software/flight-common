d6fa8b47f886af3fd311329a5a4adf7d4f337b73
Bob Whitelock <bob.whitelock1@gmail.com>
2016-08-23 13:21:30 +0100

    Adapt Header to be reusable
    Remove everything specific to FlightDeck and have this possible to be
    specified via props.

--- src/components/index.js
+++ src/components/index.js
@@ -3,7 +3,7 @@ export {createAuthorize} from './AuthorizedComponent';
 export Clipboard from './Clipboard';
 export {CustomerSupportLink, ContactCustomerSupport} from './CustomerSupport';
 export Footer from './Footer';
-// export Header from './Header'; // TODO: doesn't compile yet.
+export Header from './Header';
 export Icon from './Icon'
 export Links from './Links';
 export LoadingPage from './LoadingPage';
