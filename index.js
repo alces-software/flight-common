76594f29b8d5d05341116f0bc066eea8d8db7480
Ben Armston <ben.armston@alces-software.com>
2016-09-05 13:08:51 +0100

    Fix index.js shim

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-
-export * from 'src/index';
-
+// This shim module exists to facilitate loading flight-common from our other
+// projects in development mode.
+//
+// NOTE: This module must be valid es5.
+/* eslint-disable no-undef */
+exports = require('./src/index');
