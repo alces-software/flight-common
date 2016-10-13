1181ec5ef0533795a38313a4a029f317f206e8e8
Ben Armston <ben.armston@alces-software.com>
2016-10-13 17:04:18 +0100

    Support specifying first copyright year in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,18 +6,24 @@
  * All rights reserved, see LICENSE.txt.
  *===========================================================================*/
 import React, {PropTypes} from 'react';
-
 import moment from 'moment';
 
 class Footer extends React.Component {
   render() {
-    const {productName, content} = this.props;
-
+    const {productName, content, firstCopyrightYear} = this.props;
     const currentYear = moment().format("YYYY");
+
+    let copyrightYears;
+    if (firstCopyrightYear != null && firstCopyrightYear !== currentYear) {
+      copyrightYears = <span>{firstCopyrightYear}&ndash;{currentYear}</span>;
+    } else {
+      copyrightYears = currentYear;
+    }
+
     return (
       <footer>
         {productName} <span className="flightFooter-copyright">&copy;</span>
-        &nbsp;2015&ndash;{currentYear}&nbsp;
+        {' '}{copyrightYears}{' '}
         <a className="flightFooter-us" href="http://www.alces-flight.com">
           Alces&nbsp;Flight&nbsp;Ltd
         </a>
@@ -39,7 +45,13 @@ Footer.propTypes = {
   // TODO: May be nicer once this is used for FlightDeck to change this to an
   // array of link objects with keys for link name and address, then we can
   // generate the JSX including separators when we display these.
-  content: PropTypes.node
+  content: PropTypes.node,
+
+  firstCopyrightYear: PropTypes.string,
 }
+
+Footer.defaultProps = {
+  firstCopyrightYear: "2015",
+};
 
 export default Footer;
